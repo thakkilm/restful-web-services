4bb1d3c9115c2ac3a7169a91acb760a31d93ee4e
Mahesh <thakkilm@mail.gvsu.edu>
2023-10-09 14:51:02 -0400

    Add location uri to add user method

--- src/main/java/com/springboot/rest/weservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/springboot/rest/weservices/restfulwebservices/user/UserResource.java
@@ -1,12 +1,15 @@
 package com.springboot.rest.weservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
 @RestController
@@ -25,12 +28,16 @@ public class UserResource {
 		return service.findAll();
 	}
 	@GetMapping("/users/{id}")
-	public User retriewUser(@PathVariable int id){
+	public User retriewUser(@PathVariable int id)//@PathVariable is used to get the parameters that we set on the  URL to get the data
+	{
 		return service.findOne(id);
 	}
 	
 	@PostMapping("/users")
-	public void addNewUser(@RequestBody User user){
-			service.save(user);
-	}
+	public ResponseEntity<User> createUser(@RequestBody User user) //@RequestBody is to get the added data from POST service in talendAPI application
+	{ 
+		User newUser=service.save(user);
+		URI location=ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newUser.getId()).toUri();
+		return ResponseEntity.created(location).build();
+		}
 }
